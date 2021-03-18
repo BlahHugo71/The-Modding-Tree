05d217f6b28310be7a1a3828edf30c75babd113a
Hugo Hartono <hartonohugo@gmail.com>
2021-03-18 19:03:33 +0700

    Starting allo ver (really)
    Used a new branch tho

--- js/mod.js
+++ js/mod.js
@@ -42,18 +42,6 @@ function getPointGen() {
 		return new Decimal(0)
     }
 	var gain = new Decimal(0)
-    if (hasUpgrade("r", 11)) {
-        gain = gain.add(1)
-    }
-    if (hasUpgrade("g", 11)) {
-        gain = gain.add(1)
-    }
-    if (hasUpgrade("b", 11)) {
-        gain = gain.add(1)
-    }
-    if (hasUpgrade("b", 12)) {
-        gain = gain.mul((player.b.points.pow(0.5)).add(1))
-    }
 	return gain
 }
 
@@ -84,28 +72,22 @@ function maxTickLength() {
 function fixOldSave(oldVersion){
 }
 
-addLayer("r", {
-    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("m", {
+    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#FF0000",
+    color: "#CCCC00",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "red points", // Name of prestige currency
+    resource: "matter", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade("r", 12)) {
-            mult = mult.mul((player.r.points.pow(0.5)).add(1))
-        }
-        if (hasUpgrade("g", 12)) {
-            mult = mult.mul((player.g.points.pow(0.3)).add(1))
-        }
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -113,129 +95,7 @@ addLayer("r", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "r", description: "R: Reset for red points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "m", description: "R: Disassemble nanobots for matter", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-	infoboxes: {
-		lore: {
-			title: "red",
-			body() { return "Red points excel at boosting their own gain." },
-		},
-	},
-    upgrades: {
-        rows: 1,
-        cols: 2,
-        11: {
-            title: "Colored",
-            description: "Adds 1 to base color production.",
-            cost: new Decimal(1),
-        },
-        12: {
-            title: "Red Effect",
-            description: "Red points boost red point production",
-            cost: new Decimal(3),
-        },
-    },
-    layerShown(){return true}
-})
-
-addLayer("g", {
-    symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#00FF00",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "green points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        if (hasUpgrade("g", 12)) {
-            mult = mult.mul((player.g.points.pow(0.3)).add(1))
-        }
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "g", description: "G: Reset for green points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-	infoboxes: {
-		lore: {
-			title: "green",
-			body() {return "Green points can boost other effects greatly."},
-		},
-	},
-    upgrades: {
-        rows: 1,
-        cols: 2,
-        11: {
-            title: "Colored",
-            description: "Adds 1 to base color production.",
-            cost: new Decimal(1),
-        },
-        12: {
-            title: "Green Effect",
-            description: "Green points boost gain of all other points.",
-            cost: new Decimal(3),
-        },
-    },
-    layerShown(){return true}
-})
-
-addLayer("b", {
-    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#0000FF",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "blue points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        if (hasUpgrade("g", 12)) {
-            mult = mult.mul((player.g.points.pow(0.3)).add(1))
-        }
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "b", description: "B: Reset for blue points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-	infoboxes: {
-		lore: {
-			title: "blue",
-			body() {return "Blue points heavily boost color gain."},
-		},
-	},
-    upgrades: {
-        rows: 1,
-        cols: 2,
-        11: {
-            title: "Colored",
-            description: "Adds 1 to base color production.",
-            cost: new Decimal(1),
-        },
-        12: {
-            title: "Blue Effect",
-            description: "Blue points boost color production",
-            cost: new Decimal(3),
-        },
-    },
     layerShown(){return true}
 })
